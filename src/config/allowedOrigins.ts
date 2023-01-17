759cf5b7f3a1dfa481d557905eb7decbc2a1ce3e
Lekkyjay <lekkyjay@yahoo.com>
2023-01-17 16:46:09 +0100

    Updated cors options.

--- src/config/allowedOrigins.ts
+++ src/config/allowedOrigins.ts
@@ -2,5 +2,6 @@ export const allowedOrigins = [
   'http://localhost:3000',
   'http://localhost:5000/api',
   'https://www.dandrepairshop.com',
-  'https://dandrepairshop.com'
+  'https://dandrepairshop.com',
+  'https://booking-app-resort.netlify.app'
 ]
